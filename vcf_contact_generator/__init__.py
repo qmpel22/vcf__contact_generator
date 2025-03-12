1190736b1971600e8aa8309f9fbf33123cfaab17
Szymon <szymek42116@gmial.com>
2025-03-12 14:17:48 +0100

    test2

--- vcf_contact_generator/__init__.py
+++ vcf_contact_generator/__init__.py
@@ -1,3 +1,3 @@
-from .contacts import create_single_contact, create_library
+from vcf_contact_generator.contacts import create_single_contact, create_library
 
 __all__ = ["create_single_contact", "create_library"]
